e7979b0c2cf542a7aa4a8aa0625cb00e715e68e0
Viraat Das <viraat.laldas@gmail.com>
2018-11-22 01:18:15 -0600

    Changed the range of the game

--- guess.py
+++ guess.py
@@ -1,9 +1,9 @@
 import random
 
-randy = random.randint(1,10)
+randy = random.randint(1,20)
 a = 0
 while a != randy:
-  a = input ("guess the number that I am thinking of. Its in between 1 and 10 ")
+  a = input ("guess the number that I am thinking of. Its in between 1 and 20 ")
 
   if a == randy:
     print "gg"
